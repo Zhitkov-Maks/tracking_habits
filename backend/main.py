45585a6db173e07f3793f04d2c00d33f3a8ac08c
Maksim <m-zhitkov@inbox.ru>
2024-08-26 19:53:08 +0300

    Create and describe app fastapi

---
+++ backend/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+from starlette import status
+
+from auth.demo_auth import router
+from auth.settings import settings
+from routes.user import user_rout
+from schemas.user import ReturnUserSchema
+
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users.",
+    },
+    {
+        "name": "tweets",
+        "description": "Manage tweets.",
+    },
+    {"name": "images", "description": "Operations with images"},
+]
+
+
+app = FastAPI(
+    root_path=settings.api_v1_prefix,
+    title="TRACING HABITS APP",
+    description="API for tracking habits, helps to develop new "
+                "habits and remove destructive ones.",
+    version="0.0.1",
+    contact={
+        "name": "Maksim Zhitkov",
+        "email": "m-zhitkov@inbox.ru"
+    },
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json"
+)
+
+app.include_router(router)
+app.include_router(user_rout)
+
+
+@app.get(
+    "/user",
+    status_code=status.HTTP_200_OK,
+    response_model=ReturnUserSchema,
+    tags=["users"]
+)
+async def simple_get_data():
+    return {
+        "response": True,
+        "data": {
+            "username": "Maksim",
+            "password": "Zhitkov123",
+            "user_chat_id": 123789
+        }
+    }
