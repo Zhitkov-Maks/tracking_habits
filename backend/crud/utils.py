f4f5e3edecc48a51774f984f92f14ad7de47e1f4
Maksim <m-zhitkov@inbox.ru>
2024-08-30 22:17:45 +0300

    Add validations data

--- backend/crud/utils.py
+++ backend/crud/utils.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from routes.utils import hash_password
-from crud.user import get_user_by_chat_id
+from crud.user import get_user_by_id_and_username
 from database import User
 from database.conf_db import get_async_session
 from schemas.user import UserData
@@ -17,11 +17,34 @@ async def validate_auth_user(
     Функция для проверки существует ли пользователь в базе данных,
     и соответствует ли пароль введенному.
     """
-    user: User = await get_user_by_chat_id(
+    user: User = await get_user_by_id_and_username(
         session, login.user_chat_id, login.username
     )
     hash_pass: str = await hash_password(login.password)
     if not user or user.password != hash_pass:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail={
+                "result": False,
+                "descr": "Пользователь не найден."
+            },
+        )
+
+    return user
+
+
+async def validate_decode_user(
+    login: UserData,
+    session: AsyncSession = Depends(get_async_session)
+) -> User:
+    """
+    Функция для проверки существует ли пользователь в базе данных,
+    и соответствует ли пароль введенному.
+    """
+    user: User = await get_user_by_id_and_username(
+        session, login.user_chat_id, login.username
+    )
+    if not user or user.password != login.password:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail={
