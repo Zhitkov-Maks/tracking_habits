1e5c2597618fd13e52b65e522c189b994d7a63ea
Maksim <m-zhitkov@inbox.ru>
2024-08-30 21:13:40 +0300

    Add ListHabitsSchema

---
+++ backend/schemas/habits.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class HabitsSchema(BaseModel):
+    title: str = Field(..., min_length=3)
+    body: str =Field(..., min_length=10)
+
+
+class Habits(BaseModel):
+    title: str = Field(..., min_length=3)
+    body: str =Field(..., min_length=10)
+    start_date: datetime = None
+    is_active: bool
+
+
+class ListHabitsSchema(BaseModel):
+    data: List[Habits]
