0797138eb4a84a8f70fcff16b43b5d81e7a5e52a
Maksim <m-zhitkov@inbox.ru>
2024-08-31 17:37:06 +0300

    Implemented schema's habits

--- backend/schemas/habits.py
+++ backend/schemas/habits.py
@@ -1,21 +1,96 @@
 from datetime import datetime
 from typing import List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
-class HabitsSchema(BaseModel):
-    title: str = Field(..., min_length=3)
-    body: str =Field(..., min_length=10)
-    number_of_days: int
+class ChangeIsActiveSchema(BaseModel):
+    """Схема для изменения активности привычки."""
+    is_active: bool = Field(..., description="True или False")
 
 
-class Habits(BaseModel):
-    title: str = Field(..., min_length=3)
-    body: str =Field(..., min_length=10)
-    start_date: datetime = None
-    is_active: bool
+
+class HabitSchema(BaseModel):
+    """Схема для добавления новой привычки для отслеживания."""
+    title: str = Field(
+        ..., min_length=3, description="Краткое название привычки"
+    )
+    body: str =Field(
+        ...,
+        min_length=10,
+        description="Подробное описание, что вы хотите сделать пользователь и "
+                    "с какой целью."
+    )
+    number_of_days: int = Field(
+        ...,
+        description="Количество дней, в течении которых пользователь "
+                    "хочет отслеживать привычку."
+    )
 
 
 class ListHabitsSchema(BaseModel):
-    data: List[Habits]
+    """Список активных привычек пользователя."""
+    data: List[HabitSchema] = Field(
+        ..., description="Объект со списком привычек."
+    )
+
+
+class AddTrackSchema(BaseModel):
+    done: bool = Field(
+        ...,
+        description="Выполнено или не выполнено "
+                    "отслеживание привычки за какой-то день."
+    )
+    date: datetime = None
+
+
+class Tracking(BaseModel):
+    """Схема для отправки данных об отслеживании привычки."""
+    model_config = ConfigDict(from_attributes=True)
+    done: bool = Field(
+        ...,
+        description="Выполнено или не выполнено "
+                    "отслеживание привычки за какой-то день."
+    )
+    date: datetime = None
+    habit_id: int = Field(..., description="ID привычки")
+    id: int = Field(..., description="ID tracking")
+
+
+class FullTracking(BaseModel):
+    """
+    Схема для отправки отслеживаний с выполненными датами и невыполненными.
+    """
+    done: List[Tracking] = Field(
+        ..., description="Список с выполненными днями."
+    )
+    not_done: List[Tracking] = Field(
+        ..., description="Список с не выполненными днями."
+    )
+
+
+class HabitFull(BaseModel):
+    """
+    Схема с полной информацией о привычке. Куда входят
+    выполненные дни и невыполненные дни.
+    """
+    title: str = Field(
+        ..., min_length=3, description="Краткое название привычки"
+    )
+    body: str =Field(
+        ...,
+        min_length=10,
+        description="Подробное описание, что вы хотите сделать пользователь и "
+                    "с какой целью."
+    )
+    number_of_days: int = Field(
+        ...,
+        description="Количество дней, в течении которых пользователь "
+                    "хочет отслеживать привычку."
+    )
+    start_date: datetime = None
+    is_active: bool
+    tracking: FullTracking = Field(
+        ...,
+        description="Объект с отслеживанием дней."
+    )
