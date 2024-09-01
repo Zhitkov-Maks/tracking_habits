256e09d840da714af4f392a387307d2d58f987af
Maksim <m-zhitkov@inbox.ru>
2024-09-01 13:06:03 +0300

    Added a login and registration to the inline keyboard

--- frontend/keyboards/keyboard.py
+++ frontend/keyboards/keyboard.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 menu_bot = [
     [
-        InlineKeyboardButton(
+    InlineKeyboardButton(
             text="Добавить привычку",
             callback_data="create"
     ),
@@ -10,6 +10,16 @@ menu_bot = [
          text="Показать привычки",
          callback_data="show_habits"
      )
+    ],
+    [
+    InlineKeyboardButton(
+        text="Регистрация",
+        callback_data="registration"
+    ),
+    InlineKeyboardButton(
+            text="Вход",
+            callback_data="login"
+        )
     ],
 ]
 
