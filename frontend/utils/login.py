e6b18b8cf5745ad04045a6fb3753dd748c526b61
Maksim <m-zhitkov@inbox.ru>
2024-09-01 13:06:46 +0300

    Deleted unnecessary code

--- frontend/utils/login.py
+++ frontend/utils/login.py
@@ -1,6 +1,8 @@
 from frontend.config import jwt_token_data
 
 
-async def update_jwt_token(data: dict, user_id: int) -> dict:
-    jwt_token_data[user_id] = data
-    return jwt_token_data[user_id]
+async def update_jwt_token(
+    data: dict,
+    user_id: int
+) -> None:
+    jwt_token_data.update({user_id: data})
