85ea2a2fe8e1956a6bdc80f89ae69a0d1ba0bdd8
Maksim <m-zhitkov@inbox.ru>
2024-09-12 22:14:20 +0300

    Minor fixes and improvements

--- backend/crud/tracking.py
+++ backend/crud/tracking.py
@@ -1,7 +1,15 @@
 from datetime import datetime as dt, timedelta
 
 from fastapi import HTTPException
-from sqlalchemy import select, ScalarResult, delete, desc
+from sqlalchemy import (
+    select,
+    ScalarResult,
+    delete,
+    func,
+    Select,
+    True_,
+    False_
+)
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
@@ -23,18 +31,16 @@ async def add_tracking_for_habit(
     """
     try:
         tracking: Tracking = Tracking(
-            done=data.done,
-            date=dt.date(data.date),
-            habit_id=habit_id
+            done=data.done, date=dt.date(data.date), habit_id=habit_id
         )
         session.add(tracking)
         await session.commit()
     except IntegrityError:
-            raise HTTPException(
+        raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail={
                 "result": False,
-                "descr": "Запись на выбранную дату уже существует."
+                "descr": "Запись на выбранную дату уже существует.",
             },
         )
 
@@ -60,12 +66,11 @@ async def tracking_for_seven_days(
     """
     Возвращает данные об отслеживании конкретной привычки за последние дни.
     :param habit_id: ID Habit
-    :param done: True или False для получения списка
-    выполненных дней и невыполненных дней.
+        выполненных дней и невыполненных дней.
     :param session: AsyncSession
     """
     seven_days = (dt.now() - timedelta(days=7)).date()
-    stmt = (
+    stmt: Select = (
         select(Tracking)
         .filter(Tracking.habit_id == habit_id)
         .where(Tracking.date > seven_days)
@@ -79,26 +84,20 @@ async def tracking_for_seven_days(
 
 async def tracking_done_by_habit_id(
     habit_id: int,
-    done: True | False,
+    done: True_ | False_,
     session: AsyncSession
-) -> list:
+) -> int:
     """
     Возвращает данные об отслеживании конкретной привычки.
     :param habit_id: ID Habit
-    :param done: True или False для получения списка
-    выполненных дней и невыполненных дней.
+    :param done: True или False для получения количества
+        выполненных дней и невыполненных дней.
     :param session: AsyncSession
     """
-    stmt = (
-        select(Tracking)
-        .filter(
-            Tracking.habit_id == habit_id, Tracking.done == done
-        )
+    stmt: Select = select(func.count(Tracking.id)).filter(
+        Tracking.habit_id == habit_id, Tracking.done == done
     )
-    track: ScalarResult[Tracking] = await session.scalars(stmt)
-    if track.__sizeof__() == 0:
-        return []
-    return list(track.unique().all())
+    return await session.scalar(stmt)
 
 
 async def patch_habit_tracking(
@@ -112,19 +111,18 @@ async def patch_habit_tracking(
     :param data: Данные для обновления статуса выполнения отслеживания.
     :param session: AsyncSession
     """
-    stmt = (select(Tracking)
-            .where(Tracking.date == dt.date(data.date))
-            .where(Tracking.habit_id == habit_id))
+    stmt: Select = (
+        select(Tracking)
+        .where(Tracking.date == dt.date(data.date))
+        .where(Tracking.habit_id == habit_id)
+    )
     tracking: Tracking | None = await session.scalar(stmt)
 
     if tracking is None:
         raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail={
-            "result": False,
-            "description": "Запись не найдена."
-        },
-    )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"result": False, "description": "Запись не найдена."},
+        )
     tracking.done = data.done
     await session.commit()
 
@@ -134,6 +132,13 @@ async def check_valid_date(
     habit_id: int,
     session: AsyncSession
 ) -> None:
+    """
+    Проверяет не является ли пришедшая дата меньше даты начала
+    отслеживания привычки..
+    :param habit_id: ID Habit
+    :param data: Данные для обновления статуса выполнения отслеживания.
+    :param session: AsyncSession
+    """
     habit: Habit | None = await session.get(Habit, habit_id)
     if habit.start_date > dt.date(data.date):
         raise HTTPException(
@@ -141,6 +146,6 @@ async def check_valid_date(
             detail={
                 "result": False,
                 "descr": "Дата отметки не может быть меньше "
-                         "чем дата начала отслеживания."
+                "чем дата начала отслеживания.",
             },
         )
