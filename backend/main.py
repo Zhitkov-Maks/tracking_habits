ddf7db3a2d51c38009a2b5442545c29ad9b59b63
Maksim <m-zhitkov@inbox.ru>
2024-09-14 22:00:03 +0300

    add include router remind

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from config import settings
 from routes.habits import habits_router
+from routes.remind import remind
 from routes.tracking import track_rout
 from routes.user import user_rout
 
@@ -23,17 +24,15 @@ app = FastAPI(
     root_path=settings.api_v1_prefix,
     title="TRACING HABITS APP",
     description="API for tracking habits, helps to develop new "
-                "habits and remove destructive ones.",
+    "habits and remove destructive ones.",
     version="0.0.1",
-    contact={
-        "name": "Maksim Zhitkov",
-        "email": "m-zhitkov@inbox.ru"
-    },
+    contact={"name": "Maksim Zhitkov", "email": "m-zhitkov@inbox.ru"},
     docs_url="/api/docs",
     redoc_url="/api/redoc",
-    openapi_url="/api/openapi.json"
+    openapi_url="/api/openapi.json",
 )
 
 app.include_router(user_rout)
 app.include_router(habits_router)
 app.include_router(track_rout)
+app.include_router(remind)
