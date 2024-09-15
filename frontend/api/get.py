c8c02d350fd9a6ef69caff861efdfe4de3629f38
Maksim <m-zhitkov@inbox.ru>
2024-09-15 16:06:55 +0300

    Added docstrings

--- frontend/api/get.py
+++ frontend/api/get.py
@@ -4,9 +4,16 @@ from frontend.config import habit_url
 
 
 async def get_list_habits(
-    user_id,
+    user_id: int,
     is_active: int = 1
 ) -> dict:
+    """
+    Запрос на получения списка привычек.
+    :param user_id: Идентификатор пользователя телеграмм.
+    :param is_active: Нужен, чтобы показывать либо привычки
+        из архива либо активные.
+    :return dict: Словарь со списком привычек.
+    """
     url: str = habit_url + f"?is_active={is_active}"
     client: Client = Client(url=url)
     client.header.update(
@@ -17,8 +24,14 @@ async def get_list_habits(
 
 async def get_full_info(
     habit_id: int,
-    user_id,
+    user_id: int,
 ) -> dict:
+    """
+    Запрос на получения полной информации о конкретной привычке.
+    :param habit_id: Идентификатор привычки.
+    :param user_id: Идентификатор пользователя телеграмм.
+    :return dict: Словарь с полными данными о привычке.
+    """
     url: str = habit_url + f"{habit_id}/"
     client: Client = Client(url=url)
     client.header.update(
@@ -31,7 +44,13 @@ async def delete_habit(
     habit_id: int,
     user_id: int
 ) -> None:
-    url: str = habit_url.format(habit_id=habit_id)
+    """
+    Запрос на удаление конкретной привычки.
+    :param habit_id: Идентификатор привычки.
+    :param user_id: Идентификатор пользователя телеграмм.
+    :return None:
+    """
+    url: str = habit_url + f"{habit_id}/"
     client: Client = Client(url)
     client.header.update(
         {"Authorization": await create_header(user_id)}
@@ -44,7 +63,14 @@ async def archive_habit(
     user_id: int,
     is_active: bool
 ) -> None:
-    url: str = habit_url.format(habit_id=habit_id)
+    """
+    Запрос на изменение статуса привычки.
+    :param habit_id: Идентификатор привычки.
+    :param user_id: Идентификатор пользователя телеграмм.
+    :param is_active: Нужен, чтобы менять статус привычки.
+    :return None:
+    """
+    url: str = habit_url + f"{habit_id}/"
     client: Client = Client(url=url, data={"is_active": is_active})
     client.header.update(
         {"Authorization": await create_header(user_id)}
