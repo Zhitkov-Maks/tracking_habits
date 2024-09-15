4f19b73721620bdf05f95601abc180a1d0587b2b
Maksim <m-zhitkov@inbox.ru>
2024-09-15 13:11:49 +0300

    Added a description func gen_habit_keyword_archive

--- frontend/utils/archive.py
+++ frontend/utils/archive.py
@@ -1,19 +1,22 @@
+from typing import List
+
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 async def gen_habit_keyword_archive():
     """
     Функция генерирует клавиатуру для действия
-    по конкретной привычке.
+    по конкретной привычке в архиве.
+    :return InlineKeyboardButton: Клавиатуру с выбором действий.
     """
-    inline_actions = [
+    inline_actions: List[List[InlineKeyboardButton]] = [
         [
             InlineKeyboardButton(
                 text="Удалить",
                 callback_data="delete"
             ),
             InlineKeyboardButton(
-                text="Восстановить",
+                text="Востан-ть",
                 callback_data="un_archive"
             ),
             InlineKeyboardButton(
