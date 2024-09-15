2e508b657a150c4a9ccfd0cfe57f6dc690ea3574
Maksim <m-zhitkov@inbox.ru>
2024-09-15 12:34:21 +0300

    If there is no username, then there will be registration by first name

--- frontend/utils/register.py
+++ frontend/utils/register.py
@@ -1,6 +1,11 @@
 async def create_data(message) -> dict:
+    username: str | None = message.from_user.username
+
+    if username is None:
+        username = message.from_user.first_name
+
     return {
-        "username": message.from_user.username,
+        "username": username,
         "user_chat_id": message.from_user.id,
         "password": message.text
     }
