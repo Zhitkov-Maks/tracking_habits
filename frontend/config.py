5e3c0f202dca2094b2c662a4ef7ff84e9207a5c4
Maksim <m-zhitkov@inbox.ru>
2024-09-15 12:33:30 +0300

    Added a dictionary for storage scheduler

--- frontend/config.py
+++ frontend/config.py
@@ -8,5 +8,7 @@ register_url: str = API_ADDRESS + "/api/v1/auth/registration/"
 login_url: str = API_ADDRESS + "/api/v1/auth/login/"
 habit_url: str = API_ADDRESS + "/api/v1/habits/"
 tracking_url: str = API_ADDRESS + "/api/v1/tracking/{habit_id}/"
+remind_url: str = API_ADDRESS + "/api/v1/remind/"
 
 jwt_token_data: dict = {}
+app_schedule: dict = {}
