4e553a2485471ad160957ed0164da64e11c7230f
Maksim <m-zhitkov@inbox.ru>
2024-09-15 13:09:25 +0300

    Added a description for create_header

---
+++ frontend/utils/create.py
@@ -0,0 +1,17 @@
+from frontend.config import jwt_token_data
+
+
+
+async def create_header(user_id: int) -> str:
+    """
+    Формирование header а для отправки его в запросе для
+    аутентификации пользователя на стороне бэкенда.
+    :param user_id: Идентификатор пользователя для получения или создания
+        jwt токена.
+    :return str: Возвращает строку
+    """
+    try:
+        token: dict = jwt_token_data[user_id]
+        return f"{token.get("token_type")} {token.get("access_token")}"
+    except KeyError:
+        raise KeyError("Вы не авторизованы!")
