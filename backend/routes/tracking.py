e9e692f6f88b9697f7e95e5b569100b73dc70327
Maksim <m-zhitkov@inbox.ru>
2024-09-15 17:08:07 +0300

    Redesigned the tracking work

--- backend/routes/tracking.py
+++ backend/routes/tracking.py
@@ -3,10 +3,11 @@ from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
+from crud.check_tracking import correct_tracking, check_count_days_done
 from crud.tracking import (
-    delete_all_habit_tracking,
     patch_habit_tracking,
-    add_tracking_for_habit, check_valid_date
+    add_tracking_for_habit,
+    check_valid_date
 )
 from crud.utils import valid_decode_jwt
 from database.conf_db import get_async_session
@@ -27,12 +28,15 @@ async def add_habits_track(
     habit_id: int,
     data: AddTrackSchema,
     session: AsyncSession = Depends(get_async_session),
-    token: HTTPAuthorizationCredentials = Security(jwt_token)
+    token: HTTPAuthorizationCredentials = Security(jwt_token),
 ) -> SuccessSchema:
     """Роут добавляет отслеживание привычки."""
     await valid_decode_jwt(token.credentials, session)
     await check_valid_date(data, habit_id, session)
     await add_tracking_for_habit(habit_id, data, session)
+    await correct_tracking(data.done, habit_id, session)
+    if data.done:
+        await check_count_days_done(habit_id, session)
     return SuccessSchema(result=True)
 
 
@@ -45,30 +49,16 @@ async def add_habits_track(
     habit_id: int,
     data: AddTrackSchema,
     session: AsyncSession = Depends(get_async_session),
-    token: HTTPAuthorizationCredentials = Security(jwt_token)
-) ->SuccessSchema :
-    """Роут изменяет выполнение отслеживания. Например, не
+    token: HTTPAuthorizationCredentials = Security(jwt_token),
+) -> SuccessSchema:
+    """
+    Роут изменяет выполнение отслеживания. Например, не
     выполнено на выполнено.
     """
     await valid_decode_jwt(token.credentials, session)
     await check_valid_date(data, habit_id, session)
     await patch_habit_tracking(habit_id, data, session)
-    return SuccessSchema(result=True)
-
-
-@track_rout.delete(
-    "/{habit_id}/",
-    status_code=status.HTTP_200_OK,
-    response_model=SuccessSchema,
-)
-async def add_habits_track(
-    habit_id: int,
-    session: AsyncSession = Depends(get_async_session),
-    token: HTTPAuthorizationCredentials = Security(jwt_token)
-) -> SuccessSchema:
-    """
-    Роут чтобы, удалить всё отслеживание и начать заново.
-    """
-    await valid_decode_jwt(token.credentials, session)
-    await delete_all_habit_tracking(habit_id, session)
+    await correct_tracking(data.done, habit_id, session, patch=True)
+    if data.done:
+        await check_count_days_done(habit_id, session)
     return SuccessSchema(result=True)
