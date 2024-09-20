47c70814fb8ee7ba77da310be6a47b2c40f835f7
Maksim <m-zhitkov@inbox.ru>
2024-09-20 19:26:00 +0300

    Added description

--- frontend/utils/login.py
+++ frontend/utils/login.py
@@ -5,4 +5,10 @@ async def update_jwt_token(
     data: dict,
     user_id: int
 ) -> None:
+    """
+    Добавляет пользователя с его jwt в словарь.
+    :param data: Словарь с jwt
+    :param user_id: Идентификатор пользователя.
+    :return None:
+    """
     jwt_token_data.update({user_id: data})
