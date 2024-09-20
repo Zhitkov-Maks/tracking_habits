c0c577194c3b05f209fe9701dbf00fd0d35c42b0
Maksim <m-zhitkov@inbox.ru>
2024-09-20 20:05:40 +0300

    Minor improvements

--- frontend/utils/create.py
+++ frontend/utils/create.py
@@ -3,11 +3,11 @@ from config import jwt_token_data
 
 async def create_header(user_id: int) -> str:
     """
-    Формирование header а для отправки его в запросе для
+    Формирование header-а для отправки его в запросе для
     аутентификации пользователя на стороне бэкенда.
     :param user_id: Идентификатор пользователя для получения или создания
         jwt токена.
-    :return str: Возвращает строку
+    :return str: Возвращает строку.
     """
     try:
         token: dict = jwt_token_data[user_id]
