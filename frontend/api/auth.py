5ff0655d160b5e2b1deca32772bf3c965c4c848a
Maksim <m-zhitkov@inbox.ru>
2024-09-20 20:38:51 +0300

    added docstrings

--- frontend/api/auth.py
+++ frontend/api/auth.py
@@ -4,12 +4,22 @@ from utils.login import update_jwt_token
 
 
 async def registration(data: dict) -> None:
+    """
+    Вызов клиента для регистрации пользователя.
+    :param data: Словарь с данными пользователя.
+    :return dict: Словарь с токеном.
+    """
     client: Client = Client(register_url, data)
     await client.post()
 
 
 async def login_user(data: dict, user_id: int) -> None:
-    client: Client = Client(login_url, data)
-    response = await client.post()
-
+    """
+    Вызов клиента для аутентификации.
+    :param data: Словарь с данными пользователю
+    :param user_id: ID пользователя.
+    :return None:
+    """
+    client: Client = Client(url=login_url, data=data)
+    response: dict = await client.post()
     await update_jwt_token(response, user_id)
