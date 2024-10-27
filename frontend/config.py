5486908601e00e38b66e02b007df842e8d4cae79
Maksim <m-zhitkov@inbox.ru>
2024-10-27 14:12:00 +0300

    Fixed url address

--- frontend/config.py
+++ frontend/config.py
@@ -2,13 +2,14 @@ from decouple import config
 
 BOT_TOKEN: str = config("TOKEN")
 API_ADDRESS: str = config("API_ADDRESS")
+BASE_URL: str = "/api/v1/"
 
 
-register_url: str = API_ADDRESS + "auth/registration/"
-login_url: str = API_ADDRESS + "auth/login/"
-habit_url: str = API_ADDRESS + "habits/"
-tracking_url: str = API_ADDRESS + "tracking/{habit_id}/"
-remind_url: str = API_ADDRESS + "remind/"
+register_url: str = API_ADDRESS + BASE_URL + "auth/registration/"
+login_url: str = API_ADDRESS + BASE_URL + "auth/login/"
+habit_url: str = API_ADDRESS + BASE_URL + "habits/"
+tracking_url: str = API_ADDRESS + BASE_URL + "tracking/{habit_id}/"
+remind_url: str = API_ADDRESS + BASE_URL + "remind/"
 
 jwt_token_data: dict = {}
 app_schedule: dict = {}
