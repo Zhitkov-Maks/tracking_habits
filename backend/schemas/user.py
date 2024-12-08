a1365da1b9bd6d92f62cba1c13b727b2cec6f382
Maksim <m-zhitkov@inbox.ru>
2024-12-08 11:36:44 +0300

    Reworked database

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -5,13 +5,10 @@ from pydantic import Field
 class UserData(BaseModel):
     """
     Схема для отправки данных пользователя на сервер.
-    Username и user_chat_id берутся из данных о пользователе в телеграм.
-    Пользователь идентифицируется по user_chat_id, так как он является
-    уникальным для каждого пользователя телеграмм.
+    Пользователь идентифицируется по email, так как он является
+    уникальным для каждого пользователя.
     """
-
-    username: str = Field(..., description="Ваш username из телеграмм.")
-    user_chat_id: int = Field(..., description="Чат ай ди из телеграмм.")
+    email: str = Field(..., description="Ваш email.")
     password: str = Field(
-        ..., min_length=4, description="Пароль который ввел пользователь."
+        ..., min_length=4, description="User's password."
     )
