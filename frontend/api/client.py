8bcccf1351b937f54776263ebd505e35bcfde971
Maksim <m-zhitkov@inbox.ru>
2024-12-13 20:12:54 +0300

    Update client.py

--- frontend/api/client.py
+++ frontend/api/client.py
@@ -1,10 +1,7 @@
 from dataclasses import dataclass
+from typing import Tuple
 
 import aiohttp
-from aiohttp.client_exceptions import ClientError
-
-from api.exeptions import DateValidationError
-from http import HTTPStatus as status
 
 
 @dataclass
@@ -16,32 +13,17 @@ class Client:
     data: dict | None = None
     header = {"Content-Type": "application/json"}
 
-    async def post(self) -> dict:
+    async def post(self) -> Tuple[int, dict]:
         """Метод для POST запросов."""
         async with aiohttp.ClientSession(
-                timeout=aiohttp.ClientTimeout(60)
-        ) as client:
+                timeout=aiohttp.ClientTimeout(60)) as client:
             async with client.post(
-                url=self.url,
-                json=self.data,
-                headers=self.header
+                    url=self.url, json=self.data, headers=self.header
             ) as response:
-
                 data: dict = await response.json()
+                return response.status, data
 
-                if (response.status == status.CREATED or
-                    response.status == status.OK):
-                    return data
-
-                elif response.status == status.BAD_REQUEST:
-                    message: str = data.get("detail").get("descr")
-                    raise DateValidationError(message)
-
-                else:
-                    message: str = data.get("detail").get("descr")
-                    raise ClientError(message)
-
-    async def get(self) -> dict:
+    async def get(self) -> Tuple[int, dict]:
         """Метод для GET запросов."""
         async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(60)
@@ -52,63 +34,36 @@ class Client:
             ) as response:
 
                 data: dict = await response.json()
+                return response.status, data
 
-                if response.status == status.OK:
-                    return data
-
-                else:
-                    message: str = data.get("detail").get("descr")
-                    raise ClientError(message)
-
-    async def delete(self) -> None:
+    async def delete(self) -> Tuple[int, dict]:
         """Метод для DELETE запросов."""
         async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(60)
         ) as client:
             async with client.delete(
-                    url=self.url,
-                    headers=self.header
-            ) as response:
-
+                    url=self.url, headers=self.header) as response:
                 data: dict = await response.json()
+                return response.status, data
 
-                if response.status != status.OK:
-                    message: str = data.get("detail").get("descr")
-                    raise ClientError(message)
-
-    async def patch(self) -> None:
+    async def patch(self) -> Tuple[int, dict]:
         """Метод для PATCH запросов."""
         async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(60)
         ) as client:
             async with client.patch(
-                    url=self.url,
-                    json=self.data,
-                    headers=self.header
+                    url=self.url, json=self.data, headers=self.header
             ) as response:
-
                 data: dict = await response.json()
+                return response.status, data
 
-                if response.status != status.OK:
-                    message: str = data.get("detail").get("descr")
-                    raise ClientError(message)
-
-    async def put(self) -> dict:
+    async def put(self) -> Tuple[int, dict]:
         """Метод для PUT запросов."""
         async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(60)
         ) as client:
             async with client.put(
-                url=self.url,
-                json=self.data,
-                headers=self.header
+                url=self.url, json=self.data, headers=self.header
             ) as response:
-
                 data: dict = await response.json()
-
-                if response.status == status.OK:
-                    return data
-
-                else:
-                    message: str = data.get("detail").get("descr")
-                    raise ClientError(message)
+                return response.status, data
