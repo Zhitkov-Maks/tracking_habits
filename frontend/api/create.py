d8695c7cd2df2e0c99dd0c189f7301be266c3d71
Maksim <m-zhitkov@inbox.ru>
2024-12-16 20:02:42 +0300

    Returns a token for working with requests.

--- frontend/api/create.py
+++ frontend/api/create.py
@@ -1,16 +1,17 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 from api.client import Client
 from config import habit_url
 from utils.create import create_header
 
 
-async def request_create_habit(data: dict, user_id: int) -> None:
+async def request_create_habit(data: Dict[str, str], user_id: int) -> None:
     """
-    Вызов клиента для создания привычки.
-    :param data: Данные с привычкой.
-    :param user_id: ID пользователя чата.
-    :return None:
+    Sends a request to create a habit.
+    :param data: A dictionary with data to create a habit.
+    :param user_id: ID user.
+    :return str: If the request was not successful, then we return an
+                    error string.
     """
     client: Client = Client(url=habit_url, data=data)
     client.header.update(
--- frontend/handlers/create.py
+++ frontend/handlers/create.py
@@ -3,23 +3,20 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message
 from aiogram.utils.markdown import hbold
 from aiogram.enums import ParseMode
-from aiohttp import ClientError
 
 from api.create import request_create_habit
 from keyboards.keyboard import cancel, main_menu
 from states.add import AddState
 
-add = Router()
+add: Router = Router()
 
 
 @add.callback_query(F.data == "create")
-async def input_name_habits(
-    call: CallbackQuery,
-    state: FSMContext
-) -> None:
-    """Обработчик для ввода названия привычки."""
-    text: str = hbold("1-й этап") + ("\nВведите краткое название привычки котору "
-                                "вы хотите отследить...")
+async def input_name_habits(call: CallbackQuery, state: FSMContext) -> None:
+    """The handler asks the user for the name of the habit."""
+    text: str = hbold("1-й этап") + (
+        "\nВведите краткое название привычки которую вы хотите отследить..."
+    )
     await state.set_state(AddState.title)
     await call.message.answer(
         text=text, parse_mode="HTML", reply_markup=cancel
@@ -27,27 +24,21 @@ async def input_name_habits(
 
 
 @add.message(AddState.title)
-async def input_describe_habits(
-    mess: Message,
-    state: FSMContext
-) -> None:
-    """Обработчик для ввода описания привычки."""
+async def input_describe_habits(mess: Message, state: FSMContext) -> None:
+    """The handler asks the user for a description of the habit."""
     await state.update_data(title=mess.text)
     await state.set_state(AddState.describe)
     await mess.answer(
-        text=hbold("2-й этап") + ("\nОпишите подробнее о привычке "
-                                  "и цели которую вы хотите достичь..."),
+        text=hbold("2-й этап") + (
+            "\nОпишите подробнее о привычке и цели которую вы хотите достичь..."),
         parse_mode=ParseMode.HTML,
         reply_markup=cancel
     )
 
 
 @add.message(AddState.describe)
-async def create_and_record_db(
-        mess: Message,
-        state: FSMContext
-) -> None:
-    """Обработчик ввода количества дней для отслеживания привычки."""
+async def input_numbers_days(mess: Message, state: FSMContext) -> None:
+    """The handler asks the user for the number of days to track."""
     await state.update_data(body=mess.text)
     await state.set_state(AddState.numbers_of_days)
     await mess.answer(
@@ -58,32 +49,28 @@ async def create_and_record_db(
 
 
 @add.message(AddState.numbers_of_days, F.text.isdigit())
-async def create_and_record_db(
-        mess: Message,
-        state: FSMContext
-) -> None:
-    """Итоговое сохранение привычки."""
+async def create_and_record_db(mess: Message, state: FSMContext) -> None:
+    """The handler sends all the entered data for saving.."""
     await state.update_data(number_of_days=mess.text)
     try:
-        await request_create_habit(
+        result: None | str = await request_create_habit(
             await state.get_data(), mess.from_user.id
         )
-        await mess.answer(
-            text="Ваша привычка успешно сохранена. Не забывайте "
-                 "ежедневно выполнять ее и добавлять в отслеживание.",
-            reply_markup=main_menu
-        )
-    except (ClientError, KeyError) as err:
-        await mess.answer(
-            text=str(err),
-            reply_markup=main_menu
-        )
+        if result is None:
+            text: str = ("Ваша привычка успешно сохранена. Не забывайте "
+                         "ежедневно выполнять ее и добавлять в отслеживание.")
+        else:
+            text: str = result
+        await mess.answer(text=text, reply_markup=main_menu)
+
+    except KeyError as err:
+        await mess.answer(text=str(err), reply_markup=main_menu)
     await state.clear()
 
 
 @add.message(AddState.numbers_of_days)
 async def handler_errors(mess: Message) -> None:
-    """Обработчик если пользователь вдруг ввел не число, а строку."""
+    """Handler if the user suddenly entered a string instead of a number."""
     await mess.answer(
         text="Ошибка ввода, нужно было ввести число, попробуйте сначала.",
         reply_markup=main_menu
