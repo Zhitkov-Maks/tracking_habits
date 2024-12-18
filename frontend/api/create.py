18c10d89a132981911c96e89bca5d84f0533cf98
Maksim <m-zhitkov@inbox.ru>
2024-12-18 21:44:06 +0300

    I've reworked the code and simplified the handling of possible errors.

--- frontend/api/create.py
+++ frontend/api/create.py
@@ -1,5 +1,7 @@
 from typing import Tuple, Dict
 
+from aiohttp.abc import HTTPException
+
 from api.client import Client
 from config import habit_url
 from utils.create import create_header
@@ -14,9 +16,7 @@ async def request_create_habit(data: Dict[str, str], user_id: int) -> None:
                     error string.
     """
     client: Client = Client(url=habit_url, data=data)
-    client.header.update(
-        {"Authorization": await create_header(user_id)}
-    )
+    client.header.update({"Authorization": await create_header(user_id)})
     response: Tuple[int, dict] = await client.post()
     if response[0] != 201:
-        return response[1].get("detail").get("descr")
+        raise HTTPException(text=response[1].get("detail").get("descr"))
